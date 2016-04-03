6e1844e85ce1416e12b8d6a5e6a9fccb36ead6e9
longliuping <longliuping@live.cn>
2016-04-03 18:30:36 +0800

    push

--- ZgldStoreApi/src/com/zgld/api/test/SpringTest.java
+++ ZgldStoreApi/src/com/zgld/api/test/SpringTest.java
@@ -1,14 +1,22 @@
 package com.zgld.api.test;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.zgld.api.beans.YWxpayConfig;
 import com.zgld.api.biz.BaseBiz;
 
 public class SpringTest {
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
 		BaseBiz baseBiz = (BaseBiz) ac.getBean("baseBiz");
-		baseBiz.findAll(" from YWxpayConfig");
+		List<YWxpayConfig> list = (List<YWxpayConfig>)baseBiz.findAll(" from YWxpayConfig");
+		for (YWxpayConfig yWxpayConfig : list) {
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+		}
 	}
 }
