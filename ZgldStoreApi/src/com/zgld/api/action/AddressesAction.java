175e6157e5381bb2c24bb3296b8b8de4c9cc0396
longliuping <longliuping@live.cn>
2016-04-06 19:38:04 +0800

    push

--- ZgldStoreApi/src/com/zgld/api/action/AddressesAction.java
+++ ZgldStoreApi/src/com/zgld/api/action/AddressesAction.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.zgld.api.beans.UserShippingAddresses;
+import com.zgld.api.beans.YAccount;
+
 public class AddressesAction extends BaseAction {
 	/**
 	 * 
@@ -18,23 +21,23 @@ public class AddressesAction extends BaseAction {
 	public String delete_user_shipping_addresses() {
 		Map<String, Object> json = new HashMap<String, Object>();
 		try {
-			// AspnetUsers aspnetUsers = getUserInfo();
-			// if (aspnetUsers == null) {
-			// form.setJsonMsg(NO_USER, false, json, 201);
-			// } else {
-			// int userId = aspnetUsers.getUserId();
-			// HishopUserShippingAddresses hishopUserShippingAddresses =
-			// (HishopUserShippingAddresses)
-			// baseBiz.bean(" from HishopUserShippingAddresses as hu where hu.userId = "
-			// + userId + " and hu.shippingId = " +
-			// form.getAddress().getShippingId());
-			// if (hishopUserShippingAddresses == null) {
-			// form.setJsonMsg("要删除的地址信息不存在", false, json, 1001);
-			// } else {
-			// baseBiz.delete(hishopUserShippingAddresses);
-			// form.setJsonMsg("删除成功", true, json, 200);
-			// }
-			// }
+			YAccount account = getUserInfo();
+			if (account == null) {
+				form.setJsonMsg(NO_USER, false, json, 201);
+			} else {
+				int userId = account.getUsers().getUserId();
+				UserShippingAddresses info = (UserShippingAddresses) baseBiz
+						.bean(" from UserShippingAddresses as hu where hu.userId = "
+								+ userId
+								+ " and hu.addressId = "
+								+ form.getAddress().getAddressId());
+				if (info == null) {
+					form.setJsonMsg("要删除的地址信息不存在", false, json, 1001);
+				} else {
+					baseBiz.delete(info);
+					form.setJsonMsg("删除成功", true, json, 200);
+				}
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -51,30 +54,32 @@ public class AddressesAction extends BaseAction {
 	public String add_user_shipping_addresses() {
 		Map<String, Object> json = new HashMap<String, Object>();
 		try {
-			// HishopUserShippingAddresses info = form.getAddress();
-			// AspnetUsers aspnetUsers = getUserInfo();
-			// if (aspnetUsers == null) {
-			// form.setJsonMsg(NO_USER, false, json, 201);
-			// } else {
-			// int userId = aspnetUsers.getUserId();
-			// if (info.getRegionId() == null) {
-			// form.setJsonMsg("address.regionId不能为空", false, json, 1001);
-			// } else if (info.getShipTo() == null) {
-			// form.setJsonMsg("address.shipTo不能为空", false, json, 1001);
-			// } else if (info.getAddress() == null) {
-			// form.setJsonMsg("address.address不能为空", false, json, 1001);
-			// } else if (info.getZipcode() == null) {
-			// form.setJsonMsg("address.zipcode不能为空", false, json, 1001);
-			// } else if (info.getTelPhone() == null) {
-			// form.setJsonMsg("address.telPhone不能为空", false, json, 1001);
-			// } else if (info.getCellPhone() == null) {
-			// form.setJsonMsg("address.cellPhone不能为空", false, json, 1001);
-			// } else {
-			// info.setUserId(userId);
-			// baseBiz.save(info);
-			// form.setJsonMsg("添加成功", true, json, 200);
-			// }
-			// }
+			UserShippingAddresses info = form.getAddress();
+			YAccount account = getUserInfo();
+			if (account == null) {
+				form.setJsonMsg(NO_USER, false, json, 201);
+			} else {
+				int userId = account.getUsers().getUserId();
+				if (info.getRegionId() == null) {
+					form.setJsonMsg("address.regionId不能为空", false, json, 1001);
+				} else if (info.getShipTo() == null) {
+					form.setJsonMsg("address.shipTo不能为空", false, json, 1001);
+				} else if (info.getAddress() == null) {
+					form.setJsonMsg("address.address不能为空", false, json, 1001);
+				} else if (info.getZipcode() == null) {
+					form.setJsonMsg("address.zipcode不能为空", false, json, 1001);
+					// } else if (info.getTelPhone() == null) {
+					// form.setJsonMsg("address.telPhone不能为空", false, json,
+					// 1001);
+					// } else if (info.getCellPhone() == null) {
+					// form.setJsonMsg("address.cellPhone不能为空", false, json,
+					// 1001);
+				} else {
+					info.setUserId(userId);
+					baseBiz.save(info);
+					form.setJsonMsg("添加成功", true, json, 200);
+				}
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -91,22 +96,17 @@ public class AddressesAction extends BaseAction {
 	public String user_shipping_addresses() {
 		Map<String, Object> json = new HashMap<String, Object>();
 		try {
-			// AspnetUsers aspnetUsers = getUserInfo();
-			// if (aspnetUsers == null) {
-			// form.setJsonMsg(NO_USER, false, json, 201);
-			// } else {
-			// int userId = aspnetUsers.getUserId();
-			// List<HishopUserShippingAddresses> hishopUserShippingAddresses =
-			// (List<HishopUserShippingAddresses>)
-			// baseBiz.findAll(" from HishopUserShippingAddresses as hu where hu.userId = "
-			// + userId);
-			// for (HishopUserShippingAddresses hishopUserShippingAddresses2 :
-			// hishopUserShippingAddresses) {
-			// System.out.println(hishopUserShippingAddresses2.getAddress());
-			// }
-			// json.put(LISTINFO, hishopUserShippingAddresses);
-			// form.setJsonMsg(SUCCESS, true, json, 200);
-			// }
+			YAccount account = getUserInfo();
+			if (account == null) {
+				form.setJsonMsg(NO_USER, false, json, 201);
+			} else {
+				int userId = account.getUsers().getUserId();
+				List<UserShippingAddresses> listInfo = (List<UserShippingAddresses>) baseBiz
+						.findAll(" from UserShippingAddresses as hu where hu.userId = "
+								+ userId);
+				json.put(LISTINFO, listInfo);
+				form.setJsonMsg(SUCCESS, true, json, 200);
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -123,49 +123,50 @@ public class AddressesAction extends BaseAction {
 	public String update_user_shipping_addresses() {
 		Map<String, Object> json = new HashMap<String, Object>();
 		try {
-			// AspnetUsers aspnetUsers = getUserInfo();
-			// if (aspnetUsers == null) {
-			// form.setJsonMsg(NO_USER, false, json, 201);
-			// } else {
-			// int userId = aspnetUsers.getUserId();
-			// HishopUserShippingAddresses info = form.getAddress();
-			// if (info.getShippingId() == null) {
-			// form.setJsonMsg("address.shippingId不能为空", false, json, 1001);
-			// } else if (info.getRegionId() == null) {
-			// form.setJsonMsg("address.regionId不能为空", false, json, 1001);
-			// } else if (info.getShipTo() == null) {
-			// form.setJsonMsg("address.shipTo不能为空", false, json, 1001);
-			// } else if (info.getAddress() == null) {
-			// form.setJsonMsg("address.address不能为空", false, json, 1001);
-			// } else if (info.getZipcode() == null) {
-			// form.setJsonMsg("address.zipcode不能为空", false, json, 1001);
-			// } else if (info.getTelPhone() == null) {
-			// form.setJsonMsg("address.telPhone不能为空", false, json, 1001);
-			// } else if (info.getCellPhone() == null) {
-			// form.setJsonMsg("address.cellPhone不能为空", false, json, 1001);
-			// } else {
-			// HishopUserShippingAddresses hishopUserShippingAddresses =
-			// (HishopUserShippingAddresses)
-			// baseBiz.bean(" from HishopUserShippingAddresses as hu where hu.userId = "
-			// + userId + " and hu.shippingId = " +
-			// form.getAddress().getShippingId());
-			// if (hishopUserShippingAddresses == null) {
-			// form.setJsonMsg("要修改的地址信息不存在", false, json, 1001);
-			// } else if
-			// (!hishopUserShippingAddresses.getUserId().equals(userId)) {
-			// form.setJsonMsg("你没有权限修改", false, json, 1001);
-			// } else {
-			// hishopUserShippingAddresses.setAddress(form.getAddress().getAddress());
-			// hishopUserShippingAddresses.setCellPhone(form.getAddress().getCellPhone());
-			// hishopUserShippingAddresses.setRegionId(form.getAddress().getRegionId());
-			// hishopUserShippingAddresses.setShipTo(form.getAddress().getShipTo());
-			// hishopUserShippingAddresses.setTelPhone(form.getAddress().getTelPhone());
-			// hishopUserShippingAddresses.setZipcode(form.getAddress().getZipcode());
-			// baseBiz.update(hishopUserShippingAddresses);
-			// form.setJsonMsg("修改成功", true, json, 200);
-			// }
-			// }
-			// }
+			YAccount account = getUserInfo();
+			if (account == null) {
+				form.setJsonMsg(NO_USER, false, json, 201);
+			} else {
+				int userId = account.getUsers().getUserId();
+				UserShippingAddresses obj = form.getAddress();
+				if (obj.getAddressId() == null) {
+					form.setJsonMsg("address.addressId不能为空", false, json, 1001);
+				} else if (obj.getRegionId() == null) {
+					form.setJsonMsg("address.regionId不能为空", false, json, 1001);
+				} else if (obj.getShipTo() == null) {
+					form.setJsonMsg("address.shipTo不能为空", false, json, 1001);
+				} else if (obj.getAddress() == null) {
+					form.setJsonMsg("address.address不能为空", false, json, 1001);
+				} else if (obj.getZipcode() == null) {
+					form.setJsonMsg("address.zipcode不能为空", false, json, 1001);
+					// } else if (obj.getTelPhone() == null) {
+					// form.setJsonMsg("address.telPhone不能为空", false, json,
+					// 1001);
+					// } else if (obj.getCellPhone() == null) {
+					// form.setJsonMsg("address.cellPhone不能为空", false, json,
+					// 1001);
+				} else {
+					UserShippingAddresses info = (UserShippingAddresses) baseBiz
+							.bean(" from UserShippingAddresses as hu where hu.userId = "
+									+ userId
+									+ " and hu.addressId = "
+									+ form.getAddress().getAddressId());
+					if (info == null) {
+						form.setJsonMsg("要修改的地址信息不存在", false, json, 1001);
+					} else if (!info.getUserId().equals(userId)) {
+						form.setJsonMsg("你没有权限修改", false, json, 1001);
+					} else {
+						info.setAddress(form.getAddress().getAddress());
+						// info.setCellPhone(form.getAddress().getCellPhone());
+						info.setRegionId(form.getAddress().getRegionId());
+						info.setShipTo(form.getAddress().getShipTo());
+						// info.setTelPhone(form.getAddress().getTelPhone());
+						info.setZipcode(form.getAddress().getZipcode());
+						baseBiz.update(info);
+						form.setJsonMsg("修改成功", true, json, 200);
+					}
+				}
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
