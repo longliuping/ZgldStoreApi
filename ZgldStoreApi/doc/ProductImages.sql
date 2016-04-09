09aeea71bcdb4c0280db3a6fedf91c4c0ef9f581
longliuping <longliuping@live.cn>
2016-04-09 14:03:12 +0800

    push

---
+++ ZgldStoreApi/doc/ProductImages.sql
@@ -0,0 +1,10 @@
+INSERT INTO [ProductImages] VALUES (1, 1, 'https://gd4.alicdn.com/bao/uploaded/i4/914572036/TB2NvrBmFXXXXcDXXXXXXXXXXXX_!!914572036.jpg_400x400.jpg_.webp');
+GO
+INSERT INTO [ProductImages] VALUES (2, 1, 'https://gd4.alicdn.com/imgextra/i4/914572036/TB2YkGilXXXXXarXpXXXXXXXXXX_!!914572036.jpg_400x400.jpg_.webp');
+GO
+INSERT INTO [ProductImages] VALUES (3, 1, 'https://gd4.alicdn.com/imgextra/i4/914572036/TB2_XyBlXXXXXbZXXXXXXXXXXXX_!!914572036.jpg_400x400.jpg_.webp');
+GO
+INSERT INTO [ProductImages] VALUES (4, 1, 'https://gd1.alicdn.com/imgextra/i1/914572036/TB2yVqBlXXXXXbuXXXXXXXXXXXX_!!914572036.jpg_400x400.jpg_.webp');
+GO
+INSERT INTO [ProductImages] VALUES (5, 1, 'https://gd2.alicdn.com/imgextra/i2/914572036/TB2KriDlXXXXXa5XXXXXXXXXXXX_!!914572036.jpg_400x400.jpg_.webp');
+GO
---
+++ ZgldStoreApi/doc/SKUGroup.sql
@@ -0,0 +1,4 @@
+INSERT INTO [SKUGroup] VALUES (1, 1, 1, '颜色分类');
+GO
+INSERT INTO [SKUGroup] VALUES (2, 1, 1, '套餐');
+GO
