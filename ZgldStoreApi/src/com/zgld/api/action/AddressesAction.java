9f16d5df911651fc1c8a5846eb1a0de126606570
longliuping <longliuping@live.cn>
2016-04-18 16:32:28 +0800

    Update AddressesAction.java

--- ZgldStoreApi/src/com/zgld/api/action/AddressesAction.java
+++ ZgldStoreApi/src/com/zgld/api/action/AddressesAction.java
@@ -12,7 +12,7 @@ import com.zgld.api.beans.YAccount;
  *
  */
 public class AddressesAction extends BaseAction {
-	public void aa1122(int a,int b){
+	public void aa1122(int a,int b,int c){
 		
 	}
 	/**
