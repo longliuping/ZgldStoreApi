88b590de81421c812b23780582da80dd01f98957
longliuping <longliuping@live.cn>
2016-04-28 19:20:35 +0800

    push

--- ZgldStoreApi/src/com/zgld/api/action/RecommendAction.java
+++ ZgldStoreApi/src/com/zgld/api/action/RecommendAction.java
@@ -1,13 +1,23 @@
 package com.zgld.api.action;
 
+import java.util.HashMap;
+import java.util.Map;
 import com.zgld.api.beans.YAccount;
-import com.zgld.api.beans.YRebateRelation;
+import antlr.collections.List;
 
 public class RecommendAction extends BaseAction {
-	public String recommend1(){
+	public String recommend_user() {
+		Map json = new HashMap<>();
 		try {
-			YAccount account = getUserInfo();			
-			this.baseService.findPage(form.getPageNum(), form.getPageSize(), " from YRebateRelation as r where r.currentUserId = "+account.getUsers().getUserId());
+			YAccount account = getUserInfo();
+			if (account == null) {
+				this.form.setJsonMsg("skuId不能为空", false, json, 1001);
+			} else {
+				List listInfo = (List) this.baseService.findPage(form.getPageNum(), form.getPageSize(),
+						" from YRebateRelation as r where r.currentUserId = " + account.getUsers().getUserId());
+				json.put(LISTINFO, listInfo);
+				this.form.setJsonMsg(SUCCESS, true, json, 200);
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
