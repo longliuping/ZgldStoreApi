239dde9749ea0d1637aa6f757cea89d6eddcf6ba
longliuping <longliuping@live.cn>
2016-05-11 10:23:45 +0800

    push

---
+++ ZgldStoreApi/src/com/zgld/api/beans/AbstractOrdersRebate.java
@@ -0,0 +1,96 @@
+package com.zgld.api.beans;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * AbstractOrdersRebate entity provides the base persistence definition of the
+ * OrdersRebate entity. @author MyEclipse Persistence Tools
+ */
+
+public abstract class AbstractOrdersRebate implements java.io.Serializable {
+
+	// Fields
+
+	private Integer rebateId;
+	private Integer orderId;
+	private Integer userId;
+	private Integer shopId;
+	private Double income;
+	private Date rebateDate;
+	private String remark;
+	private Integer rebateState;
+
+	// Constructors
+
+	/** default constructor */
+	public AbstractOrdersRebate() {
+	}
+
+	// Property accessors
+
+	public Integer getRebateId() {
+		return this.rebateId;
+	}
+
+	public void setRebateId(Integer rebateId) {
+		this.rebateId = rebateId;
+	}
+
+	public Integer getOrderId() {
+		return this.orderId;
+	}
+
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+
+	public Integer getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getShopId() {
+		return this.shopId;
+	}
+
+	public void setShopId(Integer shopId) {
+		this.shopId = shopId;
+	}
+
+	public Double getIncome() {
+		return this.income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Date getRebateDate() {
+		return this.rebateDate;
+	}
+
+	public void setRebateDate(Date rebateDate) {
+		this.rebateDate = rebateDate;
+	}
+
+	public String getRemark() {
+		return this.remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public Integer getRebateState() {
+		return this.rebateState;
+	}
+
+	public void setRebateState(Integer rebateState) {
+		this.rebateState = rebateState;
+	}
+
+}
---
+++ ZgldStoreApi/src/com/zgld/api/beans/OrdersRebate.java
@@ -0,0 +1,17 @@
+package com.zgld.api.beans;
+
+import java.sql.Timestamp;
+
+/**
+ * OrdersRebate entity. @author MyEclipse Persistence Tools
+ */
+public class OrdersRebate extends AbstractOrdersRebate implements
+		java.io.Serializable {
+
+	// Constructors
+
+	/** default constructor */
+	public OrdersRebate() {
+	}
+
+}
